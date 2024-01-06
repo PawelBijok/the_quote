3a199596122c41962c4796ff408d64804ad5526a
Paweł Bijok <bijok.pawel@gmail.com>
2024-01-06 11:26:36 +0100

    Add native splash

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 To generate launcher icons modify [this file](./flutter_launcher_icons.yaml) and run following command in terminal:
 
 `dart run flutter_launcher_icons`
+
+---
+
+#### Native splash
+To generate native splash modify [this file](./flutter_native_splash.yaml) and run following command in terminal:
+
+`dart run flutter_native_splash:create`
+#### or
+`dart run flutter_native_splash:create --path=path/to/my/file.yaml`
+
+---
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:the_quote/core/app/app.dart';
 import 'package:the_quote/core/config/firebase_options.dart';
 import 'package:the_quote/core/injectable/injectable.dart';
@@ -12,7 +13,8 @@ Future<void> main() async {
 }
 
 Future<void> init() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   configureDependencies();
   await Future.wait([
     EasyLocalization.ensureInitialized(),
@@ -20,4 +22,5 @@ Future<void> init() async {
       options: DefaultFirebaseOptions.currentPlatform,
     ),
   ]);
+  FlutterNativeSplash.remove();
 }
